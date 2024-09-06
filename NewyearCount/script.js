68524b734b727c6ca95ad1939d724cbea1581c42
Rashmi Kaveesha <152951952+zrashz@users.noreply.github.com>
2024-09-06 07:50:07 +0530

    Add files via upload

---
+++ NewyearCount/script.js
@@ -0,0 +1,22 @@
+function countdown() {
+    const newYear = new Date('January 1, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = newYear - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(gap / day);
+    const hours = Math.floor((gap % day) / hour);
+    const minutes = Math.floor((gap % hour) / minute);
+    const seconds = Math.floor((gap % minute) / second);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+setInterval(countdown, 1000);
